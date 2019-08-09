994d238ee44a3eb9bfa3902161dfcb680bcbc44c
caicangcua <36462470+caicangcua@users.noreply.github.com>
2019-08-09 09:46:06 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const cors = require('cors')
 require('dotenv').load()
 const port = process.env.PORT || 3000
 
