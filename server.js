1399f55462f1baccf384049da73b13716148c407
caicangcua <36462470+caicangcua@users.noreply.github.com>
2019-08-09 08:57:53 +0700

    Update server.js

--- server.js
+++ server.js
@@ -14,8 +14,10 @@ routes(app)
 
 app.use(function (req, res) {
     //res.status(404).send({url: req.originalUrl + ' not found'})
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
     next();
 })
 
