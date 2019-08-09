9ea7bcceeda47175889d307a6d02a8a56c86ab56
caicangcua <36462470+caicangcua@users.noreply.github.com>
2019-08-09 08:24:07 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const cors = require('cors')
 require('dotenv').load()
 const port = process.env.PORT || 3000
 
@@ -16,16 +17,6 @@ app.use(function (req, res) {
     //res.status(404).send({url: req.originalUrl + ' not found'})
     res.header('Access-Control-Allow-Origin', '*');
 
-    // authorized headers for preflight requests
-    // https://developer.mozilla.org/en-US/docs/Glossary/preflight_request
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-
-    app.options('*', (req, res) => {
-        // allowed XHR methods  
-        res.header('Access-Control-Allow-Methods', 'GET, PATCH, PUT, POST, DELETE, OPTIONS');
-        res.send();
-    });
 })
 
 app.listen(port)
